dc9f1bc4cb5e24d5cc92fdf5fe4b15d56c9bbda0
casper1051 <90978578+casper1051@users.noreply.github.com>
2025-01-23 20:37:42 +0000

    Update login-authn.js

--- login-authn.js
+++ login-authn.js
@@ -5,6 +5,11 @@ login-system-fail
 loginscreen.style.position = "absolute";
 loginscreen.style.width = "100vw";
 loginscreen.style.height = "100vh";
+loginscreen.style.top="0px";
+loginscreen.style.left="0px";
+loginscreen.style.bottom="0px";
+loginscreen.style.right="0px";
+document.body.style.backgroundImage = "url('background.png')"; // Corrected syntax
 loginscreen.style.backgroundImage = "url('background.png')"; // Corrected syntax
 loginscreen.style.backgroundSize = "cover"; // Optional: Makes the image cover the entire area
 loginscreen.style.backgroundRepeat = "no-repeat"; // Optional: Prevents the image from repeating
